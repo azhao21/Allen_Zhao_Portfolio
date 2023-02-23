1982fb50b99d5f8d81f0fb9d865265b2ced27af6
azhao21 <allen07920@gmail.com>
2023-02-23 02:50:59 -0500

    carousel

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
-// get the header element
-const header = document.querySelector('header');
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+const carouselNavItems = document.querySelectorAll('.carousel-nav-item');
+const arrowLeft = document.querySelector('.carousel-arrow-left');
+const arrowRight = document.querySelector('.carousel-arrow-right');
 
-// get the distance from the top of the page to the top of the header
-const top = header.offsetTop;
+let currentIndex = 0;
+let lastIndex = carouselImages.length - 1;
 
-// add a scroll event listener to the window
-window.addEventListener('scroll', () => {
-  // if the window has been scrolled more than the distance from the top of the page to the top of the header, fix the header in place
-  if (window.pageYOffset > top) {
-    header.classList.add('fixed');
+// initialize carousel by setting the first slide to active
+carouselImages[currentIndex].parentElement.classList.add('active');
+carouselNavItems[currentIndex].classList.add('active');
+
+// add event listeners to nav items to allow direct navigation to a slide
+carouselNavItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    // remove active classes from current slide and nav item
+    carouselImages[currentIndex].parentElement.classList.remove('active');
+    carouselNavItems[currentIndex].classList.remove('active');
+
+    // set new current index and add active classes to new slide and nav item
+    currentIndex = index;
+    carouselImages[currentIndex].parentElement.classList.add('active');
+    carouselNavItems[currentIndex].classList.add('active');
+  });
+});
+
+// add event listeners to arrows to allow scrolling through the slides
+arrowLeft.addEventListener('click', () => {
+  // remove active classes from current slide and nav item
+  carouselImages[currentIndex].parentElement.classList.remove('active');
+  carouselNavItems[currentIndex].classList.remove('active');
+
+  // decrement current index and handle wraparound
+  if (currentIndex === 0) {
+    currentIndex = lastIndex;
   } else {
-    header.classList.remove('fixed');
+    currentIndex--;
   }
+
+  // add active classes to new slide and nav item
+  carouselImages[currentIndex].parentElement.classList.add('active');
+  carouselNavItems[currentIndex].classList.add('active');
+});
+
+arrowRight.addEventListener('click', () => {
+  // remove active classes from current slide and nav item
+  carouselImages[currentIndex].parentElement.classList.remove('active');
+  carouselNavItems[currentIndex].classList.remove('active');
+
+  // increment current index and handle wraparound
+  if (currentIndex === lastIndex) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+
+  // add active classes to new slide and nav item
+  carouselImages[currentIndex].parentElement.classList.add('active');
+  carouselNavItems[currentIndex].classList.add('active');
 });
